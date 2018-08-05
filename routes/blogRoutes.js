54c8486e398846c3c7a9e88c9d4e0c4680166902
Brian Fajardo <brian.fajardo@osedea.com>
2018-08-05 14:00:34 -0400

    Add Redis caching on /api/blogs GET route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
-const requireLogin = require('../middlewares/requireLogin')
+const util = require('util')
 
+const requireLogin = require('../middlewares/requireLogin')
 const Blog = mongoose.model('Blog')
+
+// Redis
+const redis = require('redis')
+const redisURL = 'redis://127.0.0.1:6379' // Default URL
+const redisClient = redis.createClient(redisURL)
 
 module.exports = app => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
@@ -14,9 +20,23 @@ module.exports = app => {
   })
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    // Wrapping Redis client.get with util.promisify to return a Promise instead of a callback.
+    redisClient.get = util.promisify(redisClient.get)
+
+    // Do we have any cached data in redis related to this query?
+    const cachedBlogs = await redisClient.get(req.user.id)
+
+    // If yes, then respond to the request right away from cache and return
+    if (cachedBlogs) return res.status(200).send(JSON.parse(cachedBlogs))
+
+    // If not, fetch data from database
     const blogs = await Blog.find({ _user: req.user.id })
 
-    res.send(blogs)
+    // Respond to the request
+    res.status(200).send(blogs)
+
+    // Update our cache
+    redisClient.set(req.user.id, JSON.stringify(blogs))
   })
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
